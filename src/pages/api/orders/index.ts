edd09700ff17717bc225524356e86851f019ea62
YARE0909 <pradyumnad092004@gmail.com>
2024-12-11 00:34:36 +0530

    Deployment Fixes

--- src/pages/api/orders/index.ts
+++ src/pages/api/orders/index.ts
@@ -1,4 +1,4 @@
-/* eslint-disable  @typescript-eslint/no-explicit-any @typescript-eslint/no-unused-vars */
+/* eslint-disable */
 
 import prisma from "@/utils/prismaClient";
 import generateRandomString from "@/utils/randomStringGenerator";
